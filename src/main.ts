c46f5dce3494056ae8e2c34618c415914fd6287a
Masquetina <iva.dopudj@gmail.com>
2016-08-07 20:02:30 +0200

    Deploy

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { AppComponent, environment } from './app/';
+import { AppComponent } from './app/';
 import { HTTP_PROVIDERS } from "@angular/http";
 import { disableDeprecatedForms, provideForms } from "@angular/forms";
 
@@ -10,9 +10,7 @@ import { AuthService } from "./app/shared/auth.service";
 import { AuthGuard } from "./app/shared/auth.guard";
 import { NonAuthGuard } from "./app/shared/non-auth.guard";
 
-if (environment.production) {
-  enableProdMode();
-}
+enableProdMode();
 
 bootstrap(AppComponent, [
   HTTP_PROVIDERS,
